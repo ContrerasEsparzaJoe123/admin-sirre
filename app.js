d5c7787001c4f7fbf2f397975d95083a24f2b2c6
ContrerasEsparzaJoe123 <47192243+ContrerasEsparzaJoe123@users.noreply.github.com>
2021-04-17 15:05:19 -0500

    initialize dogapi

--- app.js
+++ app.js
@@ -118,7 +118,7 @@ app.use("/cita", citaController);
 app.use("/nota", notaController);
 
 const PORT = process.env.PORT || 5000;
-
+dogapi.initialize(options);
 app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
